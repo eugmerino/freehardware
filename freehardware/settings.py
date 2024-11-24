8633524839e6fe48b2b00f9a3a7b20717cbd660b
Eugenio Merino <merinoeug118@gmail.com>
2024-11-24 16:47:58 -0600

    configurando debug false

--- freehardware/settings.py
+++ freehardware/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9r5^6ocd4a!=%f2%(fx6yk)+eu+k&u*)$=a2)=ucxfu*fyh#be'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'freehardware-production.up.railway.app',
