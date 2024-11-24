d6386f7c4903273878f89d769f8e8e228334b090
Eugenio Merino <merinoeug118@gmail.com>
2024-11-24 17:36:35 -0600

    eliminado STATICFILES_STORAGE

--- freehardware/settings.py
+++ freehardware/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
