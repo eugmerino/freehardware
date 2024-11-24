e0aa1731062815cb31b51efb9a3f8c24d17f608a
Eugenio Merino <merinoeug118@gmail.com>
2024-11-24 17:21:41 -0600

    agregando whitenoise

--- freehardware/settings.py
+++ freehardware/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',  # Incluye localhost para pruebas locales
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://freehardware-production.up.railway.app',]
+
 
 # Application definition
 
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
